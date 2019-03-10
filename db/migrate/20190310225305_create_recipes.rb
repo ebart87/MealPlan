3a244d338e2219da791dc135e0dbd3befb71cc0f
Erron Bartleson <ebart87@gmail.com>
2019-03-10 17:16:58 -0600

    added Ingredient, Recipe, prepOrCookingStep, and Pantry table and migrated to database

--- db/migrate/20190310225305_create_recipes.rb
+++ db/migrate/20190310225305_create_recipes.rb
@@ -1,7 +1,13 @@
 class CreateRecipes < ActiveRecord::Migration[5.1]
   def change
     create_table :recipes do |t|
-
+      t.string :name
+      t.string :meal
+      t.string :food_category
+      t.integer :cook_time
+      t.integer :prep_time
+      t.integer :number_of_ingredients
+      t.integer :number_of_steps
       t.timestamps
     end
   end
